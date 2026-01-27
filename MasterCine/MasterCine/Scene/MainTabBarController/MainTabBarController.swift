116e5c44c329e9719e8d57da1c3aae3a0d75cb2f
Caio Fabrini <caiofabrini@outlook.com>
2026-01-27 12:19:56 -0300

    adjust layout MainTabBarController

--- MasterCine/MasterCine/Scene/MainTabBarController/MainTabBarController.swift
+++ MasterCine/MasterCine/Scene/MainTabBarController/MainTabBarController.swift
@@ -12,7 +12,7 @@ final class MainTabBarController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setupTabs()
-    setupAppearance()
+    customizeTabBarAppearance()
   }
 
   private func setupTabs() {
@@ -24,19 +24,39 @@ final class MainTabBarController: UITabBarController {
     let favoritesNav = UINavigationController(rootViewController: favoritesVC)
     let profileNav = UINavigationController(rootViewController: profileVC)
 
-    homeNav.tabBarItem = UITabBarItem(title: "Home",
-                                      image: UIImage(systemName: "house"), tag: 0)
+    homeNav.tabBarItem = UITabBarItem(
+      title: "Home",
+      image: UIImage(systemName: "house")?.withRenderingMode(.alwaysTemplate),
+      selectedImage: UIImage(systemName: "house.fill")?.withRenderingMode(.alwaysTemplate)
+    )
 
-    favoritesNav.tabBarItem = UITabBarItem(title: "Favoritos",
-                                           image: UIImage(systemName: "heart"), tag: 1)
+    favoritesNav.tabBarItem = UITabBarItem(
+      title: "Favoritos",
+      image: UIImage(systemName: "heart")?.withRenderingMode(.alwaysTemplate),
+      selectedImage: UIImage(systemName: "heart.fill")?.withRenderingMode(.alwaysTemplate)
+    )
 
-    profileNav.tabBarItem = UITabBarItem(title: "Perfil",
-                                         image: UIImage(systemName: "person"), tag: 2)
+    profileNav.tabBarItem = UITabBarItem(
+      title: "Perfil",
+      image: UIImage(systemName: "person")?.withRenderingMode(.alwaysTemplate),
+      selectedImage: UIImage(systemName: "person.fill")?.withRenderingMode(.alwaysTemplate)
+    )
 
     viewControllers = [homeNav, favoritesNav, profileNav]
   }
 
-  private func setupAppearance() {
+  private func customizeTabBarAppearance() {
+    let appearance = UITabBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundEffect = nil
+    appearance.backgroundColor = .white
+    appearance.shadowColor = .separator
+
+    tabBar.standardAppearance = appearance
+    tabBar.scrollEdgeAppearance = appearance
     tabBar.isTranslucent = false
+    tabBar.itemPositioning = .fill
+    tabBar.tintColor = .red
+    tabBar.unselectedItemTintColor = .lightGray
   }
 }
